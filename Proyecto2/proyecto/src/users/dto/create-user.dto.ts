1a51cb8534e4f985967082073e996c9ca3a47a75
xRobertIng <rdrodriguezd@ul.edu.co>
2022-10-08 02:12:18 -0500

    Subiendo proyecto segundo corte Electiva

---
+++ Proyecto2/proyecto/src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+export class CreateUserDto {
+  readonly email: string;
+  password: string;
+  readonly name: string;
+  readonly lastname?: string;
+  readonly identification?: string;
+  readonly picture?: string;
+  readonly phone?: string;
+  readonly birthday?: string;
+  readonly country?: string;
+  readonly state?: string;
+  readonly city?: string;
+  readonly address?: string;
+  readonly active?: boolean;
+}
